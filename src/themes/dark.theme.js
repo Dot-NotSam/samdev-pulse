725406bdee9703038ecb5591b5494ef5ee7e7665
Sameer Prajapati <sameerprajapati066@gmail.com>
2026-02-01 19:22:08 +0530

    Update dark.theme.js

--- src/themes/dark.theme.js
+++ src/themes/dark.theme.js
@@ -8,6 +8,13 @@ const darkTheme = {
     secondaryText: '#8b949e',
     accent: '#58a6ff',
   },
+  chartColors: [
+    '#58a6ff', // Blue
+    '#3fb950', // Green
+    '#f78166', // Orange
+    '#a371f7', // Purple
+    '#ffd866', // Yellow
+  ],
 };
 
 export default darkTheme;
